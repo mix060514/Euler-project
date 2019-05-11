1f92a012fb2d6d0beeac8df1510a6f03c5631ce1
mix060514 <mix060514@gmail.com>
2019-05-11 11:11:08 +0800

    complete no.9

--- R/09.R
+++ R/09.R
@@ -1,16 +1,26 @@
+library(tidyverse)
 library(magrittr)
 
-END = 5
-a <- (1:END) ^ 2
+END = 1000 # because a + b + c = 1000, so the max of c is 1000.
+square <- (1:END) ^ 2
+solution <- data.frame(row.names = c("c", "a", "b"))
+
 for (i in 1:END) {
   for (j in 1:i) {
-    if ((a[i] - a[j]) %in% a[1:j]) {
-      print('yes')
+    if ((square[i] - square[j]) %in% square[1:j]) {
+      tmp <- c(square[i],  square[j], square[i] - square[j])
+      tmp <- sqrt(tmp)
+      solution <- rbind(solution, tmp)      
+      # print(tmp)
     } 
   }
 }
-# 
-# if (a ^ 2 + b ^ 2 == c ^ 2) {
-# 
-# }
-%>% 
+
+colnames(solution) <- c("c", "a", "b")
+head(solution)
+
+solution %>% 
+  mutate("suma" = c + b + a, 'proda' = a * b * c) %>% 
+  arrange(b) %>% 
+  select(a, b, c, suma, proda) %>% 
+  filter(suma == 1000)
